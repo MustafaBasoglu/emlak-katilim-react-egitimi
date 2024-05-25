40c64246ce917853c0d4ccb531143f535503a43e
eminbasbayan <emin.basbayan@icloud.com>
2024-05-25 10:45:48 +0300

    useState hook kullanımı

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-function App(){
-      
-    function handleClick(){
-        console.log("clicked!");
-    }
+import { useState } from "react";
 
-    return (
-        <div className="app">
-            <h1>Hello App</h1> 
-            <p id="paragraph">Ahmet</p>
-            <button id="btn" onClick={handleClick}>İsmi değiştir!</button>
-        </div> 
-    )
+function App() {
+  const [title, setTitle] = useState("Mehmet");
+  
+  let name = "Mehmet";
+  console.log(name);
+
+  function handleClick() {
+    name = "Emin Başbayan";
+    setTitle(name)
+  }
+
+  // Ekran
+  return (
+    <div className="app">
+      <h1>Hello App</h1>
+      <p id="paragraph">{title}</p>
+      <button id="btn" onClick={handleClick}>
+        İsmi değiştir!
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
