9eaeade0b33fbb4fccb57412b7ba969f5c22707d
eminbasbayan <emin.basbayan@icloud.com>
2024-05-25 10:00:12 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 function App(){
-    return <h1>Hello React!</h1>
+      
+    function handleClick(){
+        console.log("clicked!");
+    }
+
+    return (
+        <div className="app">
+            <h1>Hello App</h1> 
+            <p id="paragraph">Ahmet</p>
+            <button id="btn" onClick={handleClick}>İsmi değiştir!</button>
+        </div> 
+    )
 }
 
 export default App
