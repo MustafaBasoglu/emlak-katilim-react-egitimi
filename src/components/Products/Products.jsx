890a9a3881722e194cab42c4d316175ec0d40bae
eminbasbayan <emin.basbayan@icloud.com>
2024-05-25 14:46:03 +0300

    dinamik props işlemleri

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,29 +3,18 @@ import { productsData } from "../../productsData";
 import "./Products.css";
 
 function Products() {
-
   return (
     <div className="products">
-      <ProductItem
-        img={productsData[0].image}
-        title={productsData[0].title}
-        price={productsData[0].price}
-      />
-      <ProductItem
-        img={productsData[1].image}
-        title={productsData[1].title}
-        price={productsData[1].price}
-      />
-      <ProductItem
-        img={productsData[2].image}
-        title={productsData[2].title}
-        price={productsData[2].price}
-      />
-      <ProductItem
-        img={productsData[3].image}
-        title={productsData[3].title}
-        price={productsData[3].price}
-      />
+      {productsData.map((product) => {
+        return (
+          <ProductItem
+            key={product.id}
+            img={product.image}
+            title={product.title}
+            price={product.price}
+          />
+        );
+      })}
     </div>
   );
 }
