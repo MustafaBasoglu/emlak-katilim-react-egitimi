f0dd1e575aaf2a7e41cd69fbd97fc18aa2b59661
eminbasbayan <emin.basbayan@icloud.com>
2024-05-25 11:15:19 +0300

    counter tek function örneği

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,22 +3,31 @@ import { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
-  function arttir() {
-    setCount(count + 1);
-  }
+  //   function arttir() {
+  //     setCount(count + 1);
+  //   }
 
-  function azalt(){
-    if(count === 0){
-        return;
+  //   function azalt(){
+  //     if(count === 0){
+  //         return;
+  //     }
+  //     setCount(count - 1);
+  //   }
+
+  function handleClick(params) {
+    if (params === "+") {
+      setCount(count + 1);
+    } else {
+      if (count === 0) return;
+      setCount(count - 1);
     }
-    setCount(count - 1);
   }
 
   return (
     <div className="counter">
-      <button onClick={arttir}>+</button>
+      <button onClick={() => handleClick("+")}>+</button>
       <strong>{count}</strong>
-      <button onClick={azalt}>-</button>
+      <button onClick={() => handleClick("-")}>-</button>
     </div>
   );
 }
