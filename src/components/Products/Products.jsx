25fecb13683cdfaeb00f11fd361d0e629561f133
eminbasbayan <emin.basbayan@icloud.com>
2024-05-25 14:15:31 +0300

    react componentinde javascript dosyası çalıştırma

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,17 +1,30 @@
 import ProductItem from "./ProductItem";
+import { productsData } from "../../productsData";
 import "./Products.css";
 
 function Products() {
-  const img = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
-  const title = "Şapka";
-  const price = 500;
 
   return (
     <div className="products">
       <ProductItem
-        img={img}
-        title={title}
-        price={price}
+        img={productsData[0].image}
+        title={productsData[0].title}
+        price={productsData[0].price}
+      />
+      <ProductItem
+        img={productsData[1].image}
+        title={productsData[1].title}
+        price={productsData[1].price}
+      />
+      <ProductItem
+        img={productsData[2].image}
+        title={productsData[2].title}
+        price={productsData[2].price}
+      />
+      <ProductItem
+        img={productsData[3].image}
+        title={productsData[3].title}
+        price={productsData[3].price}
       />
     </div>
   );
