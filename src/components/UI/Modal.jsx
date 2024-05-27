f81e01739ac52206604c5c2ab6cfbdf1ef2d4c50
eminbasbayan <emin.basbayan@icloud.com>
2024-05-27 14:12:07 +0300

    click out side işlemi

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -8,34 +8,36 @@ const Modal = ({ title, children, isShowModal, setIsShowModal }) => {
   }
 
   return (
-    <div className="modal d-inline-block">
-      <div className="modal-dialog">
-        <div className="modal-content bg-dark">
-          <div className="modal-header">
-            <h5 className="modal-title">{title}</h5>
-            <button
-              type="button"
-              className="btn-close"
-              onClick={() => setIsShowModal(false)}
-            ></button>
-          </div>
-          <div className="modal-body">
-            <p>{children}</p>
-          </div>
-          <div className="modal-footer">
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={() => setIsShowModal(false)}
-            >
-              Close
-            </button>
-            <button type="button" className="btn btn-primary">
-              Save changes
-            </button>
-          </div>
+    <div className="modal d-inline-flex flex-column justify-content-center align-items-center">
+      <div className="modal-content bg-dark z-3 position-relative w-50">
+        <div className="modal-header">
+          <h5 className="modal-title">{title}</h5>
+          <button
+            type="button"
+            className="btn-close"
+            onClick={() => setIsShowModal(false)}
+          ></button>
+        </div>
+        <div className="modal-body">
+          <p>{children}</p>
+        </div>
+        <div className="modal-footer">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => setIsShowModal(false)}
+          >
+            Close
+          </button>
+          <button type="button" className="btn btn-primary">
+            Save changes
+          </button>
         </div>
       </div>
+      <div
+        className="w-100 h-100 position-absolute top-0 left-0 bg-danger opacity-25 z-0"
+        onClick={() => setIsShowModal(false)}
+      ></div>
     </div>
   );
 };
