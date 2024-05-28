ac7e900a65e7586f80b32fd84f3203b733a1f7d1
eminbasbayan <emin.basbayan@icloud.com>
2024-05-28 10:43:19 +0300

    useContext hook kullanımı

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import PropTypes from "prop-types";
+import { CartContext } from "../../context/CartContext";
 import "./Button.css";
 
 function Button(props) {
   const { type, size, onClick, children, className } = props;
   const classNames = `${className} btn btn-${type} btn-${size}`;
+  const data = useContext(CartContext);
+
+  console.log(data);
 
   return (
     <button className={classNames} onClick={onClick}>
