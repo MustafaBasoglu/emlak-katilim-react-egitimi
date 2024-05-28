78c4216cfde543bda36be24be48273e968a3c9b4
eminbasbayan <emin.basbayan@icloud.com>
2024-05-28 14:07:56 +0300

    NavLink component

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { ThemeContext } from "../../context/ThemeContext";
 import { themes } from "../../context/ThemeProvider";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   const { cartItems } = useContext(CartContext);
@@ -16,38 +16,35 @@ const Header = () => {
           <Link className="navbar-brand" to="/">
             My Website
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+          <button className="navbar-toggler" type="button">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto fs-5">
               <li className="nav-item">
-                <Link to={"/"} className="nav-link active">
+                <NavLink
+                  to={"/"}
+                  className={({ isActive }) =>
+                    `nav-link ${isActive && "active"}`
+                  }
+                >
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/products">
+                <NavLink className="nav-link" to="/products">
                   Products
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <NavLink className="nav-link" to="/about">
                   About
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/cart">
+                <NavLink className="nav-link" to="/cart">
                   Cart
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
                 <Link className="nav-link position-relative" to="/cart">
