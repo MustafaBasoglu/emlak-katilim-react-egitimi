0e8a9f20e61ed20f18c1924c70c89f4e89135cce
eminbasbayan <emin.basbayan@icloud.com>
2024-05-28 09:35:21 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { ToastContainer } from "react-toastify";
 import HomePage from "./pages/HomePage";
 import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="app">
       <main className="container">
+        <Header />
         <HomePage />
       </main>
       <ToastContainer />
