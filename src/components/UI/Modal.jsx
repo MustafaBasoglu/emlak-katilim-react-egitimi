f3a73af43e23e3b23fc25a421821e954ffd19f63
eminbasbayan <emin.basbayan@icloud.com>
2024-05-28 09:23:14 +0300

    clean up function setInterval örneği

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,16 +1,26 @@
 import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 //! react componentleri kendine bağlı state ve props güncellendiğinde yeniden render olur!
 
 const Modal = ({ title, children, setIsShowModal }) => {
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
+    let i = 0;
+    console.log("modal DOM'a yüklendi!");
+
+    const id = setInterval(()=>{
+    i += 1;
+    setCount(i);
+    console.log(i);
+    }, 1000)
     
     //* clean-up function - ekrandan kaldırıldığında çalışır
     return () => {
       console.log("modal DOM'dan kaldırıldı!");
+      clearInterval(id);
     };
   }, []);
 
@@ -18,6 +28,7 @@ const Modal = ({ title, children, setIsShowModal }) => {
     <div className="modal d-inline-flex flex-column justify-content-center align-items-center">
       <div className="modal-content bg-dark z-3 position-relative w-50">
         <div className="modal-header">
+          <h5 className="modal-title">{count}</h5>
           <h5 className="modal-title">{title}</h5>
           <button
             type="button"
