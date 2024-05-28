dfe7e62421680a3d0a55e92a55bb9d080b15d4d8
eminbasbayan <emin.basbayan@icloud.com>
2024-05-28 12:30:22 +0300

    çoklu route tanımlama

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,18 @@ import HomePage from "./pages/HomePage";
 import Header from "./components/Layout/Header";
 import "react-toastify/dist/ReactToastify.css";
 import ProductsPage from "./pages/ProductsPage";
+import AboutPage from "./pages/AboutPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   const { theme } = useContext(ThemeContext);
 
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomePage />
-    },
-    {
-      path: "/products",
-      element: <ProductsPage />
-    }
-  ])
+    { path: "/", element: <HomePage /> },
+    { path: "/products", element: <ProductsPage /> },
+    { path: "/about", element: <AboutPage /> },
+    { path: "/cart", element: <CartPage /> },
+  ]);
 
   return (
     <div
---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,41 @@
+const CartPage = () => {
+  return (
+    <div className="cart-page">
+      <h1>Cart Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dolorum
+        repudiandae quas minima voluptates itaque quaerat voluptatem
+        reprehenderit, nisi nulla accusantium odit error neque maiores dolores.
+        Magni, quo ratione? Magnam, dolorum excepturi? Inventore magni quo
+        tempore tempora quibusdam fugit ex. Veritatis, magni vel odit temporibus
+        ex in necessitatibus neque?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dolorum
+        repudiandae quas minima voluptates itaque quaerat voluptatem
+        reprehenderit, nisi nulla accusantium odit error neque maiores dolores.
+        Magni, quo ratione? Magnam, dolorum excepturi? Inventore magni quo
+        tempore tempora quibusdam fugit ex. Veritatis, magni vel odit temporibus
+        ex in necessitatibus neque?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dolorum
+        repudiandae quas minima voluptates itaque quaerat voluptatem
+        reprehenderit, nisi nulla accusantium odit error neque maiores dolores.
+        Magni, quo ratione? Magnam, dolorum excepturi? Inventore magni quo
+        tempore tempora quibusdam fugit ex. Veritatis, magni vel odit temporibus
+        ex in necessitatibus neque?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis dolorum
+        repudiandae quas minima voluptates itaque quaerat voluptatem
+        reprehenderit, nisi nulla accusantium odit error neque maiores dolores.
+        Magni, quo ratione? Magnam, dolorum excepturi? Inventore magni quo
+        tempore tempora quibusdam fugit ex. Veritatis, magni vel odit temporibus
+        ex in necessitatibus neque?
+      </p>
+    </div>
+  );
+};
+
+export default CartPage;
