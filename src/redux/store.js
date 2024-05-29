f400099e3fa31baa9889f2b49f67dab8cb675396
eminbasbayan <emin.basbayan@icloud.com>
2024-05-29 15:19:05 +0300

    authSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice";
 import cartReducer from "./slices/cartSlice";
 import themeReducer from "./slices/themeSlice";
+import authReducer from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     cart: cartReducer,
     theme: themeReducer,
-    auth: {},
+    auth: authReducer,
   },
 });
 
